04a607be906b98d27d9f483f1b724d0cbf4ef5a8
ToufikGouri <toufikgouri3@gmail.com>
2024-06-10 19:52:55 +0530

    Initialization with frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <div className='text-red-700 bg-green-200'>
+        <h1>This is full stack beginning</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
