d4d6191d9e027474e303a7c8331c24ab996c7413
ToufikGouri <toufikgouri3@gmail.com>
2024-06-13 12:55:51 +0530

    add cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,12 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 // configs
 app.use(express.json({ limit: "16kb" }))                            // configure data in json 
